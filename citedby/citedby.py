551a8673497b0e633c2a20e57c33e26814f8fb00
fabiobatalha <fabiobatalha@gmail.com>
2014-04-04 10:10:30 -0300

    Corrigindo problema com slashs no final da url.

--- citedby/citedby.py
+++ citedby/citedby.py
@@ -8,20 +8,27 @@ import pymongo
 import utils
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
-from pyramid.view import view_config
+from pyramid.view import view_config, notfound_view_config
 from pyramid.response import Response
 
 from controller import query_by_pid, query_by_doi, query_by_meta
+
+
+@notfound_view_config(append_slash=True)
+def notfound(request):
+    return HTTPNotFound('Not found')
+
 
 @view_config(route_name='index', request_method='GET')
 def index(request):
     return Response('Cited by SciELO API')
 
+
 @view_config(route_name='citedby_pid', request_method='GET', renderer='json')
 def citedby_pid(request):
     if not 'q' in request.GET:
         return None
-    
+
     articles = query_by_pid(request.db['articles'], request.GET['q'])
 
     return articles
@@ -31,22 +38,26 @@ def citedby_pid(request):
 def citedby_doi(request):
     if not 'q' in request.GET:
         return None
-    
+
     articles = query_by_doi(request.db['articles'], request.GET['q'])
 
     return articles
+
 
 @view_config(route_name='citedby_meta', request_method='GET', renderer='json')
 def citedby_meta(request):
     if not 'title' in request.GET:
         return None
-    
-    articles = query_by_meta(request.db['articles'],
+
+    articles = query_by_meta(
+        request.db['articles'],
         title=request.GET.get('title', ''),
         author=request.GET.get('author', ''),
-        year=request.GET.get('year', ''))
+        year=request.GET.get('year', '')
+    )
 
     return articles
+
 
 def main(settings, *args, **xargs):
     config_citedby = Configurator(settings=settings)
@@ -74,6 +85,10 @@ config = utils.Configuration.from_file(os.environ['CONFIG_INI'])
 
 settings = dict(config.items())
 app = main(settings)
-if __name__== '__main__':
-    server = make_server(settings['http_server']['ip'], int(settings['http_server']['port']), app)
+if __name__ == '__main__':
+    server = make_server(
+        settings['http_server']['ip'],
+        int(settings['http_server']['port']),
+        app
+    )
     server.serve_forever()
