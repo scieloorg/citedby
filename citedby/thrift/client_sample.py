5be0eef0a79596aecd333cd6d0b69aae0907c381
Jamil Atta Junior <atta.jamil@gmail.com>
2015-03-30 15:02:21 -0300

    Corrigindo path para o thrft file

--- citedby/thrift/client_sample.py
+++ citedby/thrift/client_sample.py
@@ -4,19 +4,19 @@
 import os
 import thriftpy
 
-
 from thriftpy.rpc import make_client
-
-citedby_thrift = thriftpy.load(os.path.join(os.path.dirname(
-                               os.path.abspath(__file__)), 'citedby.thrift'))
-
-client = make_client(citedby_thrift.Citedby, '127.0.0.1', 11610)
 
 
 #Examples using thrift
+if __name__ == '__main__':
 
-print client.citedbypid('S1516-89132010000300001', False)
+    citedby_thrift = thriftpy.load(os.path.join(os.path.dirname(
+                                   os.path.abspath(__file__)), 'citedby.thrift'))
 
-print client.citedbydoi('10.1590/S1516-89132010000300001')
+    client = make_client(citedby_thrift.Citedby, '127.0.0.1', 11610)
 
-print client.citedbymeta(title='Biochemical and morphological changes during the growth kinetics of Araucaria angustifolia suspension cultures')
+    print client.citedbypid('S1516-89132010000300001', False)
+
+    print client.citedbydoi('10.1590/S1516-89132010000300001')
+
+    print client.citedbymeta(title='Biochemical and morphological changes during the growth kinetics of Araucaria angustifolia suspension cultures')
