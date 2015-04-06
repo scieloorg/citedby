0e1a4f54e06a5176a4229ef545c32ea7b0bbe56c
Jamil Atta Junior <atta.jamil@gmail.com>
2015-04-06 15:42:37 -0300

    Memcached no servidor thrift

--- citedby/thrift/citedby.thrift
+++ citedby/thrift/citedby.thrift
@@ -3,9 +3,9 @@ exception ServerError{
 }
 
 service Citedby{
-    string citedbypid(1:required string q, 2:bool metaonly) throws (1:ServerError error_message)
+    string citedby_pid(1:required string q, 2:bool metaonly) throws (1:ServerError error_message)
 
-    string citedbydoi(1:required string q, 2:bool metaonly) throws (1:ServerError error_message)
+    string citedby_doi(1:required string q, 2:bool metaonly) throws (1:ServerError error_message)
 
-    string citedbymeta(1:required string title, 2:string author_surname, 3:i32 year, 4:bool metaonly) throws (1:ServerError error_message)
+    string citedby_meta(1:required string title, 2:string author_surname, 3:i32 year, 4:bool metaonly) throws (1:ServerError error_message)
 }
--- citedby/thrift/server.py
+++ citedby/thrift/server.py
@@ -4,19 +4,24 @@
 import os
 import json
 import thriftpy
+from thriftpy.thrift import TProcessor
 from ConfigParser import SafeConfigParser
 
-from thriftpy.thrift import TProcessor
 
 from citedby.icitation import ICitation
-from citedby.icontroller import (
-                                query_by_pid,
-                                query_by_doi,
-                                query_by_meta
-                                )
+from dogpile.cache import make_region
+from citedby.icontroller import (query_by_pid,
+                                 query_by_doi,
+                                 query_by_meta)
+
+from citedby.utils import key_generator
+
 
 citedby_thrift = thriftpy.load(os.path.join(os.path.dirname(
                                os.path.abspath(__file__)), 'citedby.thrift'))
+
+cache_region = make_region(name="citedby_trhift",
+                           function_key_generator=key_generator)
 
 
 class Dispatcher(object):
@@ -29,41 +34,56 @@ class Dispatcher(object):
         config.readfp(open(
             os.path.dirname(__file__) + '/../../production.ini'))
 
-        settings = dict(config.items('app:main'))
+        self.settings = dict(config.items('app:main'))
 
         self.index = ICitation(
-            hosts=settings['elasticsearch_host'].split())
+            hosts=self.settings['elasticsearch_host'].split())
+
+        cache_region.configure('dogpile.cache.bmemcached',
+            expiration_time=int(self.settings['memcached_expiration_time']),
+            arguments={'url':self.settings['memcached_arguments_url'].split()})
 
 
-    def citedbypid(self, q, metaonly):
+    def citedby_pid(self, q, metaonly):
 
-        try:
-            return json.dumps(query_by_pid(self.index, q, metaonly))
-        except:
-            return citedby_thrift.ServerError(
-                            'Server Error: icontroller.query_by_pid(%s, %s, %s)'
-                             % (self.index, q, metaonly)).message
+        @cache_region.cache_on_arguments(namespace=self.settings['memcached_prefix_thrift'])
+        def _citedby_pid(q, metaonly):
+            try:
+                return json.dumps(query_by_pid(self.index, q, metaonly))
+            except:
+                return citedby_thrift.ServerError(
+                                'Server Error: icontroller.query_by_pid(%s, %s, %s)'
+                                 % (self.index, q, metaonly)).message
 
-
-    def citedbydoi(self, q, metaonly):
-
-        try:
-            return json.dumps(query_by_doi(self.index, q, metaonly))
-        except:
-            return citedby_thrift.ServerError(
-                            'Server Error: icontroller.query_by_doi(%s, %s, %s)'
-                            % (self.index, q, metaonly)).message
+        return _citedby_pid(q, metaonly)
 
 
-    def citedbymeta(self, title, author_surname, year, metaonly):
+    def citedby_doi(self, q, metaonly):
 
-        try:
-            return json.dumps(
-                query_by_meta(self.index, title, author_surname, year, metaonly))
-        except:
-            return citedby_thrift.ServerError(
-                'Server Error: icontroller.citedbymeta(%s, %s, %s, %s, %s)'
-                 % (self.index, title, author_surname, year, metaonly)).message
+        @cache_region.cache_on_arguments(namespace=self.settings['memcached_prefix_thrift'])
+        def _citedby_doi(q, metaonly):
+            try:
+                return json.dumps(query_by_doi(self.index, q, metaonly))
+            except:
+                return citedby_thrift.ServerError(
+                                'Server Error: icontroller.query_by_doi(%s, %s, %s)'
+                                % (self.index, q, metaonly)).message
+
+        return _citedby_doi(q, metaonly)
+
+
+    def citedby_meta(self, title, author_surname, year, metaonly):
+
+        @cache_region.cache_on_arguments(namespace=self.settings['memcached_prefix_thrift'])
+        def _citedby_meta(title, author_surname, year, metaonly):
+            try:
+                return json.dumps(
+                    query_by_meta(self.index, title, author_surname, year, metaonly))
+            except:
+                return citedby_thrift.ServerError(
+                    'Server Error: icontroller.citedbymeta(%s, %s, %s, %s, %s)'
+                     % (self.index, title, author_surname, year, metaonly)).message
+        return _citedby_meta(title, author_surname, year, metaonly)
 
 
 app = TProcessor(citedby_thrift.Citedby, Dispatcher())
